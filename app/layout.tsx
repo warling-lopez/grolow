bbff4e1335638054ee5128ff7e9ed89de8d1ce1f
warling-lopez <warlinglopez01@gmail.com>
2026-06-14 03:31:48 -0400

    feat: add Google Analytics scripts for tracking and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import ClientLayout from '@/app/components/ClientLayout';
 import './globals.css';
 
@@ -43,6 +44,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           {children}
         </ClientLayout>
       </body>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-Q8RNVKKBJZ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-Q8RNVKKBJZ');
+        `}
+      </Script>
     </html>
   );
 }
